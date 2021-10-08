09c1694100272a23a4651293911cbffff7059fa2
David Zeman <davca.zeman@gmail.com>
2021-10-08 12:36:06 +0200

    Add project files.

---
+++ LAB/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+namespace LAB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Debugger.IsAttached && args?.Length == 0)
+            {
+                args = PocitaniSlov.GetArgs();
+            }
+
+            PocitaniSlov.Run(args);
+        }
+    }
+}
