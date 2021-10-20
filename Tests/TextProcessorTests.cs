2790e77aea1037afb70b48168bcc383413131659
David Zeman <David.zeman@outlook.com>
2021-10-20 11:34:22 +0200

    DataRow do testu

--- Tests/TextProcessorTests.cs
+++ Tests/TextProcessorTests.cs
@@ -9,42 +9,15 @@ namespace LAB.Tests
     public class TextProcessorTests
     {
         [TestMethod]
-        public void MissingArgumentsTest()
+        [DataRow(new string[0])]
+        [DataRow(new string[] { "fileIn.txt" })]
+        [DataRow(new string[] { "fileIn.txt", "fileOut.txt" })]
+        [DataRow(new string[] { "fileIn.txt", "fileOut.txt", "a" })]
+        public void WrongArgumentsTest(string[] args)
         {
             StringWriter writer = new StringWriter();
             Console.SetOut(writer);
 
-            string[] args = new string[0];
-            TextProcessorHelper.RunAlignContent(args);
-
-            StringReader reader = new StringReader(writer.ToString());
-            string firstLine = reader.ReadLine();
-
-            Assert.AreEqual(TextProcessor.ERROR_ARGUMENT, firstLine);
-        }
-
-        [TestMethod]
-        public void WrongArguments1Test()
-        {
-            StringWriter writer = new StringWriter();
-            Console.SetOut(writer);
-
-            string[] args = new string[] { "file1.txt" };
-            TextProcessorHelper.RunAlignContent(args);
-
-            StringReader reader = new StringReader(writer.ToString());
-            string firstLine = reader.ReadLine();
-
-            Assert.AreEqual(TextProcessor.ERROR_ARGUMENT, firstLine);
-        }
-
-        [TestMethod]
-        public void WrongArguments2Test()
-        {
-            StringWriter writer = new StringWriter();
-            Console.SetOut(writer);
-
-            string[] args = new string[] { "file1.txt", "file2.txt" };
             TextProcessorHelper.RunAlignContent(args);
 
             StringReader reader = new StringReader(writer.ToString());
