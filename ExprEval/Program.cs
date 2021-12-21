751ed7754e945f16fe861c24a1d94b7ad2b94fb1
David Zeman <davca.zeman@gmail.com>
2021-12-21 18:20:29 +0100

    Objektovy navrh pro vyraz

--- ExprEval/Program.cs
+++ ExprEval/Program.cs
@@ -3,16 +3,158 @@ using System.Collections.Generic;
 
 namespace ExprEval
 {
-    internal class Program
+    public class ExpressionEvaluationStatus
     {
-        private enum OperatorArityEnum
+        public StateEnum State { get; set; }
+
+        public ExpressionEvaluationStatus(StateEnum state)
         {
-            None = 0,
-            Unary = 1,
-            Binary = 2,
+            State = state;
         }
 
-        private static bool CharIsOperator(char c, out OperatorArityEnum arity)
+        public enum StateEnum
+        {
+            Ok = 0,
+            FormatError = 1,
+            DivideByZero = 2,
+            OverflowError = 3,
+        }
+
+        public override string ToString()
+        {
+            switch (State)
+            {
+                case StateEnum.Ok: return "";
+                case StateEnum.FormatError: return "Format Error";
+                case StateEnum.DivideByZero: return "Divide Error";
+                case StateEnum.OverflowError: return "Overflow Error";
+                default: return "";
+            }
+        }
+    }
+
+    /// <summary>
+    /// Class for working with numerical expressions
+    /// </summary>
+    public class Expression
+    {
+        /// <summary>
+        /// Expression raw text
+        /// </summary>
+        public string Text { get; protected set; }
+
+        #region CONSTRUCTORS
+
+        public Expression(string text)
+        {
+            this.Text = text;
+        }
+
+        #endregion
+
+        #region PUBLIC METHODS
+
+        /// <summary>
+        /// Evaluate integer expression with safty checks for division by zero, formatting etc...If error occurrs, returns null and status.
+        /// </summary>
+        /// <param name="status">Status</param>
+        /// <returns>Expression result or null, if error occurrs.</returns>
+        public int? EvaluateIntSafe(out ExpressionEvaluationStatus status)
+        {
+            status = new ExpressionEvaluationStatus(ExpressionEvaluationStatus.StateEnum.Ok);
+            Stack<int> stack = new Stack<int>();
+            try
+            {
+                for (int i = Text.Length - 1; i >= 0; i--)
+                {
+                    char c = Text[i];
+
+                    if (CharIsOperator(c, out OperatorArityEnum arity))
+                    {
+                        int result = 0;
+                        if (arity == OperatorArityEnum.Unary)
+                        {
+                            int num1 = stack.Pop();
+                            switch (c)
+                            {
+                                case '~':
+                                    result = checked(-num1);
+                                    break;
+                            }
+                        }
+                        else if (arity == OperatorArityEnum.Binary)
+                        {
+                            int num1 = stack.Pop();
+                            int num2 = stack.Pop();
+                            switch (c)
+                            {
+                                case '+':
+                                    result = checked(num1 + num2);
+                                    break;
+                                case '-':
+                                    result = checked(num1 - num2);
+                                    break;
+                                case '*':
+                                    result = checked(num1 * num2);
+                                    break;
+                                case '/':
+                                    if (num2 == 0)
+                                    {
+                                        status.State = ExpressionEvaluationStatus.StateEnum.DivideByZero;
+                                        return null;
+                                    }
+                                    result = checked(num1 / num2);
+                                    break;
+                            }
+                        }
+                        stack.Push(result);
+                    }
+                    else
+                    {
+                        int? num = ReadInt(ref i, ref status);
+                        if (status.State != ExpressionEvaluationStatus.StateEnum.Ok)
+                        {
+                            return null;
+                        }
+                        else if (num != null)
+                        {
+                            stack.Push(num.Value);
+                        }
+                    }
+                }
+            }
+            catch (OverflowException)
+            {
+                status.State = ExpressionEvaluationStatus.StateEnum.OverflowError;
+                return null;
+            }
+            catch
+            {
+                throw;
+            }
+
+            if (stack.Count == 1)
+            {
+                return stack.Peek();
+            }
+            else 
+            {
+                status.State = ExpressionEvaluationStatus.StateEnum.FormatError;
+                return null;
+            }
+        }
+
+        #endregion
+
+        #region PRIVATE METHODS
+
+        /// <summary>
+        /// Check if char is operator and return its arity
+        /// </summary>
+        /// <param name="c">Character</param>
+        /// <param name="arity">Operator arity</param>
+        /// <returns>true if valid operator; otherwise false</returns>
+        private bool CharIsOperator(char c, out OperatorArityEnum arity)
         {
             arity = OperatorArityEnum.Unary;
             bool isUnary = c == '~';
@@ -24,85 +166,61 @@ namespace ExprEval
             return isUnary || isBinary;
         }
 
+        /// <summary>
+        /// Reads int from Text starting at position i
+        /// </summary>
+        /// <param name="i">Position in Text</param>
+        /// <returns>Read integer</returns>
+        /// <exception cref="FormatException">If value in Text is to large for Int32</exception>
+        private int? ReadInt(ref int i, ref ExpressionEvaluationStatus status)
+        {
+            string buffer = "";
+            while (i >= 0 && Text[i] != ' ')
+            {
+                buffer = Text[i] + buffer;
+                i--;
+            }
+            if (buffer != "")
+            {
+                try
+                {
+                    return int.Parse(buffer);
+                }
+                catch (OverflowException)
+                {
+                    status.State = ExpressionEvaluationStatus.StateEnum.FormatError;
+                }
+            }
+            return null;
+        }
+
+        #endregion
+
+        #region ENUMS
+
+        /// <summary>
+        /// Operator arity
+        /// </summary>
+        private enum OperatorArityEnum
+        {
+            None = 0,
+            Unary = 1,
+            Binary = 2,
+        }
+
+        #endregion
+    }
+
+    internal class Program
+    {
         private static void Main(string[] args)
         {
             try
             {
-                string expr = Console.ReadLine();
-                Stack<int> stack = new Stack<int>();
-
-                for (int j = expr.Length - 1; j >= 0; j--)
-                {
-                    char c = expr[j];
-
-                    if (CharIsOperator(c, out OperatorArityEnum arity))
-                    {
-                        if (arity == OperatorArityEnum.Unary)
-                        {
-                            int num1 = stack.Pop();
-                            switch (c)
-                            {
-                                case '~':
-                                    stack.Push(checked(-num1));
-                                    break;
-                            }
-                        }
-                        else if (arity == OperatorArityEnum.Binary)
-                        {
-                            int num1 = stack.Pop();
-                            int num2 = stack.Pop();
-                            switch (c)
-                            {
-                                case '+':
-                                    stack.Push(checked(num1 + num2));
-                                    break;
-                                case '-':
-                                    stack.Push(checked(num1 - num2));
-                                    break;
-                                case '*':
-                                    stack.Push(checked(num1 * num2));
-                                    break;
-                                case '/':
-                                    stack.Push(checked(num1 / num2));
-                                    break;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        string buffer = "";
-                        while (j >= 0 && expr[j] != ' ')
-                        {
-                            buffer = expr[j] + buffer;
-                            j--;
-                        }
-                        if (buffer != "")
-                        {
-                            try
-                            {
-                                stack.Push(int.Parse(buffer));
-                            }
-                            catch (OverflowException ex)
-                            {
-                                throw new FormatException("Invalid expression format", ex);
-                            }
-                        }
-                    }
-                }
-
-                if (stack.Count == 1)
-                {
-                    Console.WriteLine(stack.Peek());
-                }
-                else throw new FormatException("Invalid expression format");
-            }
-            catch (OverflowException)
-            {
-                Console.WriteLine("Overflow Error");
-            }
-            catch (DivideByZeroException)
-            {
-                Console.WriteLine("Divide Error");
+                string expression = Console.ReadLine();
+                int? result = new Expression(expression).EvaluateIntSafe(out ExpressionEvaluationStatus status);
+                if (status.State == ExpressionEvaluationStatus.StateEnum.Ok) Console.WriteLine(result);
+                else Console.WriteLine(status.ToString());
             }
             catch
             {
